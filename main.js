f4db4b8cd7ff9f5fd85d3218c38b5684acfd36a6
Ayush Sharma <sharma.ayush3103@gmail.com>
2020-04-29 20:23:41 +0530

    final commit

--- main.js
+++ main.js
@@ -31,13 +31,50 @@ document.getElementById('ban-up').addEventListener('click',function(){
     }
 });
 
-
+var j=-1;
 document.getElementById('price1').addEventListener('click',function(){
     document.getElementById('price1').style.backgroundColor='#04f804';
     document.getElementById('price2').style.backgroundColor='transparent';
+    document.getElementById('total').innerHTML=parseInt(0);
+    document.getElementById('pay').innerHTML=parseInt(0);
+    j=1;
 });
 document.getElementById('price2').addEventListener('click',function(){
     document.getElementById('price2').style.backgroundColor='#04f804';
     document.getElementById('price1').style.backgroundColor='transparent';
+    document.getElementById('total').innerHTML=parseInt(0);
+    document.getElementById('pay').innerHTML=parseInt(0);
+    j=0;
+});
+
+var i=0;
+document.getElementById('plus').addEventListener('click',function(){
+    if(j==1 || j==0){
+    document.getElementById('total').innerHTML=++i;
+    if(j==1){
+        document.getElementById('pay').innerHTML=parseInt(i)*399;  
+    }
+    else if(j==0){
+        document.getElementById('pay').innerHTML=parseInt(i)*299;  
+    }
+    }
+});
+
+document.getElementById('minus').addEventListener('click',function(){
+    if(i<0){
+        document.getElementById('total').innerHTML=parseInt(0);
+        document.getElementById('pay').innerHTML=parseInt(0);
+    }
+    else{
+        if(j==1 || j==0){ 
+        document.getElementById('total').innerHTML=--i;
+        if(j==1){
+            document.getElementById('pay').innerHTML=parseInt(i)*399;  
+        }
+        else if(j==0){
+            document.getElementById('pay').innerHTML=parseInt(i)*299;  
+        }
+        }
+}
 });
 
